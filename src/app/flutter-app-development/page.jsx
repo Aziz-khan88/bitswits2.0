d4c5dde9150e2d4cde9cdbbf4fd8822e88d54879
rabia-dev <dev.ra@productions-dev.com>
2024-09-04 20:53:43 +0500

    dynamic data call"

--- src/app/flutter-app-development/page.jsx
+++ src/app/flutter-app-development/page.jsx
@@ -1,13 +1,9 @@
-import Achieved from "../services/achieved";
-import Banner from "../services/banner";
-import {BannerData, Awards, AwardContent} from "@/src/app/flutter-app-development/data/data";
+
 
 export default function Page() {
   return (
     <>
-      <Banner data={BannerData} />
-      <Achieved slides={Awards} options={OPTIONS} />
-      <Achieved slides={Awards} options={OPTIONS} data={AwardContent} />
+ 
     </>
   );
 }
