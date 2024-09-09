35750027525e989f5bd22e938ced91f83ab412af
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-09-09 23:04:06 +0500

    Images changes

--- src/app/mobile-game-development-company/data/data.jsx
+++ src/app/mobile-game-development-company/data/data.jsx
@@ -4,13 +4,13 @@ import BannerLogos from "media/mobile-game-development-company/bannerLogo.webp"
 import BanngBg from "media/mobile-game-development-company/bannerIMG.webp"
 
 // services Images
-import GameBg1 from "media/mobile-game-development-company/services/gameBg1.webp"
-import GameBg2 from "media/mobile-game-development-company/services/gameBg2.webp"
-import GameBg3 from "media/mobile-game-development-company/services/gameBg3.webp"
-import GameBg4 from "media/mobile-game-development-company/services/gameBg4.webp"
-import GameBg5 from "media/mobile-game-development-company/services/gameBg5.webp"
-import GameBg6 from "media/mobile-game-development-company/services/gameBg6.webp"
-import GameBg7 from "media/mobile-game-development-company/services/gameBg7.webp"
+import GameBg1 from "media/mobile-game-development-company/services/GameBg1.webp"
+import GameBg2 from "media/mobile-game-development-company/services/GameBg2.webp"
+import GameBg3 from "media/mobile-game-development-company/services/GameBg3.webp"
+import GameBg4 from "media/mobile-game-development-company/services/GameBg4.webp"
+import GameBg5 from "media/mobile-game-development-company/services/GameBg5.webp"
+import GameBg6 from "media/mobile-game-development-company/services/GameBg6.webp"
+import GameBg7 from "media/mobile-game-development-company/services/GameBg7.webp"
 
 //========= Testimonial Images
 import TestiImg1 from "media/services/reviwes/trustpilot.webp"
